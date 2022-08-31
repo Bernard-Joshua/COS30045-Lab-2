2c63d7bf68029ca43786ba0b5980c5ff597aed03
Bernard Joshua Raja Rajan <67912425+Bernard-Joshua@users.noreply.github.com>
2022-08-31 21:11:09 +0800

    Updates ( Final )

---
+++ Scripts/loading_csv.js
@@ -0,0 +1,28 @@
+d3.csv("/Data/wombats.csv", function (data) {
+	console.log(data);
+	wombatSightings = data;
+
+	// Calling the function.
+	barChart(wombatSightings);
+});
+
+function barChart(dataset) {
+	const w = 500;
+	const h = 100;
+
+	const svg = d3
+		.select("#chart") // Reference the id of the HTML element not the body.
+		.append("svg")
+		.attr("width", w)
+		.attr("height", h);
+
+	svg.selectAll("rect")
+		.data(dataset)
+		.enter()
+		.append("rect")
+		.attr("x", (d, i) => i * (w / dataset.length))
+		.attr("y", (d, i) => h - 4 * d.wombats) // Reference the column-name to get the values.
+		.attr("width", 35)
+		.attr("height", (d, i) => 4 * d.wombats)
+		.attr("fill", "navy");
+}
